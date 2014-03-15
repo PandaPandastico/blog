d6ba93529e1491d5eece04ca485058920930e252
Pandy <jmsg1984@gmail.com>
2014-03-15 23:21:40 +0000

    Add validate test

--- spec/features/posts_create_spec.rb
+++ spec/features/posts_create_spec.rb
@@ -10,4 +10,12 @@ describe "Create post" do
     find("input[type='submit']").click
     Post.where(title: "Cucamonga").count.should == 1
   end
+  it "should validate title and text length" do
+    page.driver.browser.basic_authorize('dhh', 'secret')
+    visit new_post_path
+    fill_in "post_title", with: "Test"
+    fill_in "post_text", with: ""
+    find("input[type='submit']").click
+    page.has_css?('error_explanation')
+  end
 end
--- spec/features/posts_update_spec.rb
+++ spec/features/posts_update_spec.rb
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 require "spec_helper"
 
 describe "Update post" do
@@ -10,5 +9,13 @@ describe "Update post" do
     fill_in "post_text", with: "uCBHYTgygutddldffoadsoi"
     find("input[type='submit']").click
     Post.where(title: "Cucamonga").count.should == 1
+  end
+  it "should validate title and text length" do
+    page.driver.browser.basic_authorize('dhh', 'secret')
+    visit new_post_path
+    fill_in "post_title", with: "Test"
+    fill_in "post_text", with: ""
+    find("input[type='submit']").click
+    page.has_css?('error_explanation')
   end
 end
